706010066e840b0eced2242fa56617ef0b439852
HamBeomJoon <hbj0802@naver.com>
2024-04-21 01:22:58 +0900

    LifeCycle 예외 처리, 예제 완성

--- Android_Basic_Study/Android_Basic_Study_03/app/src/main/java/com/example/android_basic_study_03/MainActivity.kt
+++ Android_Basic_Study/Android_Basic_Study_03/app/src/main/java/com/example/android_basic_study_03/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.example.android_basic_study_03
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.android_basic_study_03.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestGalleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null && data.data != null) {
+                    val selectedImageUri: Uri = data.data!!
+                    Glide.with(this)
+                        .load(selectedImageUri)
+                        .into(binding.ivTest)
+                } else {
+                    Toast.makeText(this, "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "갤러리 액티비티를 실행하지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(this.binding.root)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        // 기본적으로 인터넷의 사진 불러오기
+//        Glide.with(this)
+//            .load("https://media.vlpt.us/images/sasy0113/post/f7085683-1a62-4ce7-9f7f-e8fd2f3ec825/Android%20Kotlin.jpg")
+//            .into(binding.ivTest)
+        binding.btnGallery.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            requestGalleryLauncher.launch(intent)
+        }
+
     }
+
 }
