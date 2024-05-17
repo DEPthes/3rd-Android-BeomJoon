b4012dee40cf97890484aac7f5e671259ad742f2
HamBeomJoon <hbj0802@naver.com>
2024-05-17 21:44:09 +0900

    [6주차 과제] itemRecyclerView

---
+++ Android_Basic_Study/Android_Basic_Study_05/app/src/main/java/com/example/app/data/Item.kt
@@ -0,0 +1,9 @@
+package com.example.app.data
+
+import android.widget.ImageView
+
+data class Item(
+    val imageUrl: String,
+    val title: String,
+    val price: Int,
+)
---
+++ Android_Basic_Study/Android_Basic_Study_05/app/src/main/java/com/example/app/presentation/ItemRvAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.app.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.app.data.Item
+import com.example.app.databinding.ItemListBinding
+
+class ItemRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var itemList = ArrayList<Item>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = ItemListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ItemHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ItemHolder) {
+            holder.bind(itemList[position])
+        }
+    }
+
+    fun setData(list: ArrayList<Item>) {
+        itemList = list
+        notifyDataSetChanged()
+    }
+
+    inner class ItemHolder(val binding: ItemListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            Glide.with(binding.root.context)
+                .load(item.imageUrl)
+                .override(60, 60)
+                .into(binding.ivThumbnail)
+
+            binding.tvItemName.text = item.title
+            binding.tvItemPrice.text = item.price.toString()
+        }
+    }
+}
