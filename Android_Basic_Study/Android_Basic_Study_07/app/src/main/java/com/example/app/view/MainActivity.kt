e86fac6855df6a78522cd204696043e83b7ff834
HamBeomJoon <hbj0802@naver.com>
2024-06-21 12:04:01 +0900

    detailFragment에서 NavBar 숨김

--- Android_Basic_Study/Android_Basic_Study_07/app/src/main/java/com/example/app/view/MainActivity.kt
+++ Android_Basic_Study/Android_Basic_Study_07/app/src/main/java/com/example/app/view/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.app.view
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.app.R
 import com.example.app.databinding.ActivityMainBinding
 import com.example.app.view.card.CardFragment
 import com.example.app.view.home.HomeFragment
-import com.example.app.view.home.HomeViewModel
-import com.example.app.view.utils.UiState
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -41,13 +38,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // 프래그먼트 교체하는 함수
+    // 프래그먼트 replace
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(binding.flMain.id, fragment).commit()
     }
 
     fun replaceFragmentWithBackstack(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(binding.flMain.id, fragment)
-            .addToBackStack(null).commit()
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+            .add(binding.flMain.id, fragment) // `add`를 사용하여 프래그먼트 위에 겹치도록 함
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun hideNavBar() {
+        binding.bn.visibility = View.GONE
+    }
+
+    fun showNavBar() {
+        binding.bn.visibility = View.VISIBLE
     }
 }
--- Android_Basic_Study/Android_Basic_Study_07/app/src/main/java/com/example/app/view/detail/DetailFragment.kt
+++ Android_Basic_Study/Android_Basic_Study_07/app/src/main/java/com/example/app/view/detail/DetailFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.app.databinding.FragmentDetailBinding
+import com.example.app.view.MainActivity
 import com.example.app.view.utils.UiState
 
 class DetailFragment(private val photoId: String) : Fragment() {
@@ -22,6 +23,14 @@ class DetailFragment(private val photoId: String) : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(layoutInflater)
+
+        // NavBar 숨기기
+        (activity as MainActivity).hideNavBar()
+
+        binding.ivExit.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+            (activity as MainActivity).showNavBar()
+        }
 
         observer()
         detailViewModel.fetchData(photoId)
@@ -46,10 +55,18 @@ class DetailFragment(private val photoId: String) : Fragment() {
                     binding.tvPhotoDesc.text = it.data.description
                     binding.tvPhotoLikes.text = it.data.likes.toString()
                     binding.tvPhotoDownloads.text = it.data.downloads.toString()
-                    binding.tvPhotoTags.text = "tags"
+                    binding.tvPhotoTags.text = it.data.tags.joinToString(" ") { tag -> "#$tag" }
+
+                    // 북마크 불투명도 설정
+                    setBookmarkOpacity(it.data.isBookmark)
                 }
             }
         }
+    }
+
+    private fun setBookmarkOpacity(isBookmark: Boolean) {
+        val opacity = if (isBookmark) 0.3f else 1.0f
+        binding.ivBookmark.alpha = opacity
     }
 
     override fun onDestroyView() {
